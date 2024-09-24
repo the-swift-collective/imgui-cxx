markvilar/imgui

Texture-based round corners: Tweaks, renaming to match tex lines branch.

imgui_draw.cpp
@@ -1027,11 +1027,11 @@ void ImDrawList::AddLine(const ImVec2& p1, const ImVec2& p2, ImU32 col, float th
 // We are using the textures generated by ImFontAtlasBuildRenderRoundCornersTexData()
 inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImVec2& b, ImU32 col, float rounding, float thickness, ImDrawCornerFlags rounding_corners_flags, bool fill)
 {
-    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+    if (!(draw_list->Flags & ImDrawListFlags_RoundCornersUseTex)) // Disabled by the draw list flags
         return false;
 
     const ImDrawListSharedData* data = draw_list->_Data;
-    IM_ASSERT_PARANOID(!(data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoTexturedRoundCorners)); // No data in font
+    IM_ASSERT_PARANOID(!(data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoRoundCorners)); // No data in font
 
     // Filled rectangles have no stroke width
     const int stroke_width = fill ? 1 : (int)thickness;
@@ -1042,7 +1042,7 @@ inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImV
     const int rad = (int)rounding + (stroke_width - 1);
 
     // We don't support zero radius
-    if ((rad <= 0) || (rad > ImFontAtlasRoundCornersMaxSize))
+    if (rad <= 0 || rad > ImFontAtlasRoundCornersMaxSize)
         return false; // We can't handle this
 
     const unsigned int index = (stroke_width - 1) + ((rad - 1) * ImFontAtlasRoundCornersMaxStrokeWidth);
@@ -1055,10 +1055,10 @@ inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImV
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
 
     // Calculate UVs for the three points we are interested in from the texture
-    // corner_uv[0] is the innermost point of the circle (solid for filled circles)
-    // corner_uv[1] is either straight down or across from it (depending on if we are using the filled or stroked version)
-    // corner_uv[2] is diagonally across from it
-    // corner_uv[1] is always solid (either inside the circle or on the line), whilst corner_uv[2] is always blank
+    // - corner_uv[0] is the innermost point of the circle (solid for filled circles)
+    // - corner_uv[1] is either straight down or across from it (depending on if we are using the filled or stroked version)
+    // - corner_uv[2] is diagonally across from it
+    // - corner_uv[1] is always solid (either inside the circle or on the line), whilst corner_uv[2] is always blank
     // This represents a 45 degree "wedge" of circle, which then gets mirrored here to produce a 90 degree curve
     // See ImFontAtlasBuildRenderRoundCornersTexData() for more details of the texture contents
     // If use_alternative_uvs is true then this means we are drawing a stroked texture that has been packed into the "filled"
@@ -1473,13 +1473,13 @@ void ImDrawList::AddTriangleFilled(const ImVec2& p1, const ImVec2& p2, const ImV
 // (in which case the caller should try the regular circle drawing code)
 inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, float radius, float thickness, ImU32 col, bool fill)
 {
-    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+    if (!(draw_list->Flags & ImDrawListFlags_RoundCornersUseTex)) // Disabled by the draw list flags
         return false;
 
     const ImDrawListSharedData* data = draw_list->_Data;
     ImTextureID tex_id = data->Font->ContainerAtlas->TexID;
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
-    IM_ASSERT_PARANOID(!(data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoTexturedRoundCorners)); // No data in font
+    IM_ASSERT_PARANOID(!(data->Font->ContainerAtlas->Flags & ImFontAtlasFlags_NoRoundCorners)); // No data in font
 
     // Filled rectangles have no stroke width
     const int stroke_width = fill ? 1 : (int)thickness;
@@ -2844,7 +2844,7 @@ static void ImFontAtlasBuildRegisterRoundCornersCustomRects(ImFontAtlas* atlas)
 {
     if (atlas->TexRoundCornerData.Size > 0)
         return;
-    if ((atlas->Flags & ImFontAtlasFlags_NoTexturedRoundCorners))
+    if (atlas->Flags & ImFontAtlasFlags_NoRoundCorners)
         return;
 
     const int pad = FONT_ATLAS_ROUNDED_CORNER_TEX_PADDING;
@@ -2864,7 +2864,7 @@ static void ImFontAtlasBuildRegisterRoundCornersCustomRects(ImFontAtlas* atlas)
             ImFontRoundedCornerData corner_data;
             if (ImFontAtlasRoundCornersStrokeWidthMask & (1 << stroke_width_index))
             {
-                if ((stroke_width_index == 0) || (spare_rect_id < 0))
+                if (stroke_width_index == 0 || spare_rect_id < 0)
                 {
                     corner_data.RectId = atlas->AddCustomRectRegular(width, height);
                     corner_data.StrokedUsesAlternateUVs = false;
@@ -2894,7 +2894,7 @@ static void ImFontAtlasBuildRegisterRoundCornersCustomRects(ImFontAtlas* atlas)
 static void ImFontAtlasBuildRenderRoundCornersTexData(ImFontAtlas* atlas)
 {
     IM_ASSERT(atlas->TexPixelsAlpha8 != NULL);
-    if ((atlas->Flags & ImFontAtlasFlags_NoTexturedRoundCorners))
+    if ((atlas->Flags & ImFontAtlasFlags_NoRoundCorners))
         return;
 
     // Render the texture
@@ -4123,6 +4123,7 @@ void ImGui::RenderColorRectWithAlphaCheckerboard(ImDrawList* draw_list, ImVec2 p
         draw_list->AddRectFilled(p_min, p_max, col, rounding, rounding_corners_flags);
     }
 }
+
 
 //-----------------------------------------------------------------------------
 // [SECTION] Decompression code

the-swift-collective/imgui-cxx

imgui_draw.cpp
@@ -1420,7 +1420,7 @@ void ImDrawList::AddLine(const ImVec2& p1, const ImVec2& p2, ImU32 col, float th
 // We are using the textures generated by ImFontAtlasBuildRenderRoundCornersTexData()
 inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImVec2& b, ImU32 col, float rounding, float thickness, ImDrawFlags flags, bool fill)
 {
-    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+    if (!(draw_list->Flags & ImDrawListFlags_RoundCornersUseTex)) // Disabled by the draw list flags
         return false;
 
 #if 1
@@ -1441,7 +1441,7 @@ inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImV
     const int rad = (int)rounding + (stroke_width - 1);
 
     // We don't support zero radius
-    if ((rad <= 0) || (rad > ImFontAtlasRoundCornersMaxSize))
+    if (rad <= 0 || rad > ImFontAtlasRoundCornersMaxSize)
         return false; // We can't handle this
 
     const unsigned int index = (stroke_width - 1) + ((rad - 1) * ImFontAtlasRoundCornersMaxStrokeWidth);
@@ -1454,10 +1454,10 @@ inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImV
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
 
     // Calculate UVs for the three points we are interested in from the texture
-    // corner_uv[0] is the innermost point of the circle (solid for filled circles)
-    // corner_uv[1] is either straight down or across from it (depending on if we are using the filled or stroked version)
-    // corner_uv[2] is diagonally across from it
-    // corner_uv[1] is always solid (either inside the circle or on the line), whilst corner_uv[2] is always blank
+    // - corner_uv[0] is the innermost point of the circle (solid for filled circles)
+    // - corner_uv[1] is either straight down or across from it (depending on if we are using the filled or stroked version)
+    // - corner_uv[2] is diagonally across from it
+    // - corner_uv[1] is always solid (either inside the circle or on the line), whilst corner_uv[2] is always blank
     // This represents a 45 degree "wedge" of circle, which then gets mirrored here to produce a 90 degree curve
     // See ImFontAtlasBuildRenderRoundCornersTexData() for more details of the texture contents
     // If use_alternative_uvs is true then this means we are drawing a stroked texture that has been packed into the "filled"
@@ -1878,7 +1878,7 @@ void ImDrawList::AddTriangleFilled(const ImVec2& p1, const ImVec2& p2, const ImV
 // (in which case the caller should try the regular circle drawing code)
 inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, float radius, float thickness, ImU32 col, bool fill)
 {
-    if (!(draw_list->Flags & ImDrawListFlags_TexturedRoundCorners)) // Disabled by the draw list flags
+    if (!(draw_list->Flags & ImDrawListFlags_RoundCornersUseTex)) // Disabled by the draw list flags
         return false;
 
     const ImDrawListSharedData* data = draw_list->_Data;
@@ -3812,7 +3812,7 @@ static void ImFontAtlasBuildRegisterRoundCornersCustomRects(ImFontAtlas* atlas)
 {
     if (atlas->TexRoundCornerData.Size > 0)
         return;
-    if ((atlas->Flags & ImFontAtlasFlags_NoBakedRoundCorners))
+    if (atlas->Flags & ImFontAtlasFlags_NoBakedRoundCorners)
         return;
 
     const int pad = FONT_ATLAS_ROUNDED_CORNER_TEX_PADDING;
@@ -3832,7 +3832,7 @@ static void ImFontAtlasBuildRegisterRoundCornersCustomRects(ImFontAtlas* atlas)
             ImFontRoundedCornerData corner_data;
             if (ImFontAtlasRoundCornersStrokeWidthMask & (1 << stroke_width_index))
             {
-                if ((stroke_width_index == 0) || (spare_rect_id < 0))
+                if (stroke_width_index == 0 || spare_rect_id < 0)
                 {
                     corner_data.RectId = atlas->AddCustomRectRegular(width, height);
                     corner_data.StrokedUsesAlternateUVs = false;
@@ -5184,6 +5184,7 @@ void ImGui::RenderColorRectWithAlphaCheckerboard(ImDrawList* draw_list, ImVec2 p
         draw_list->AddRectFilled(p_min, p_max, col, rounding, flags);
     }
 }
+
 
 //-----------------------------------------------------------------------------
 // [SECTION] Decompression code