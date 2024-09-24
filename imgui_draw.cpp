markvilar/imgui

Texture-based round corners: Minor tweaks

imgui_draw.cpp
@@ -1023,6 +1023,7 @@ void ImDrawList::AddLine(const ImVec2& p1, const ImVec2& p2, ImU32 col, float th
 // should be drawn rounded
 // Returns true if the rectangle was drawn, false for some reason it couldn't
 // be (in which case the caller should try again with the regular path drawing API)
+// We are using the textures generated by ImFontAtlasBuildRenderRoundCornersTexData()
 inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImVec2& b, ImU32 col, float rounding, ImDrawCornerFlags rounding_corners_flags, bool fill)
 {
     const ImDrawListSharedData* data = draw_list->_Data;
@@ -1289,13 +1290,8 @@ void ImDrawList::AddRect(const ImVec2& p_min, const ImVec2& p_max, ImU32 col, fl
 
     // Try to use fast path if we can
     if (rounding > 0)
-    {
         if (AddRoundCornerRect(this, p_min, p_max, col, rounding, rounding_corners, /* fill */ false))
-        {
-            // Fast path handled this
             return;
-        }
-    }
 
     if (Flags & ImDrawListFlags_AntiAliasedLines)
         PathRect(p_min + ImVec2(0.50f,0.50f), p_max - ImVec2(0.50f,0.50f), rounding, rounding_corners);
@@ -1316,14 +1312,10 @@ void ImDrawList::AddRectFilled(const ImVec2& p_min, const ImVec2& p_max, ImU32 c
     {
         // Try fast path first
         if (AddRoundCornerRect(this, p_min, p_max, col, rounding, rounding_corners, /* fill */ true))
-        {
             return;
-        }
-        else
-        {
-            PathRect(p_min, p_max, rounding, rounding_corners);
-            PathFillConvex(col);
-        }
+
+        PathRect(p_min, p_max, rounding, rounding_corners);
+        PathFillConvex(col);
     }
     else
     {
@@ -1404,8 +1396,7 @@ inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, fl
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
 
     const int rad = (int)radius;
-    if ((rad < 1) || // Radius 0 will cause issues with the UV lookup below
-        (rad > data->Font->ContainerAtlas->RoundCornersMaxSize))
+    if (rad < 1 || rad > data->Font->ContainerAtlas->RoundCornersMaxSize) // Radius 0 will cause issues with the UV lookup below
         return false; // We can't handle this
 
     // Debug command to force this render path to only execute when shift is held
@@ -1437,7 +1428,6 @@ inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, fl
 
     // Some useful constants for our calculations
     const float half_sqrt_two = 0.70710678f; // sqrtf(2.0f) * 0.5f
-    const float sqrt_two_minus_one = 0.41421356f; // sqrt(2.0f) - 1.0f
     const float width_offset_parametric = line_width / rad; // Line width in our parametric coordinate space
 
     const int num_verts = fill ? 9 : 16; // Number of vertices we are going to write
@@ -2758,7 +2748,11 @@ static void ImFontAtlasBuildRegisterRoundCornersCustomRects(ImFontAtlas* atlas)
     const int pad = FONT_ATLAS_ROUNDED_CORNER_TEX_PADDING;
     const int max = atlas->RoundCornersMaxSize;
     for (int n = 0; n < max; n++)
-        atlas->RoundCornersRectIds.push_back(atlas->AddCustomRectRegular(n + 1 + pad * 2, n + 1 + FONT_ATLAS_ROUNDED_CORNER_TEX_CENTER_PADDING  + pad * 2));
+    {
+        const int width = n + 1 + pad * 2;
+        const int height = n + 1 + FONT_ATLAS_ROUNDED_CORNER_TEX_CENTER_PADDING + pad * 2;
+        atlas->RoundCornersRectIds.push_back(atlas->AddCustomRectRegular(width, height));
+    }
 }
 
 // Generate the actual pixel data for rounded corners in the atlas
@@ -2811,7 +2805,6 @@ static void ImFontAtlasBuildRenderRoundCornersTexData(ImFontAtlas* atlas)
                 }
 
                 const float dist = ImSqrt((float)(cx*cx+cy*cy)) - (float)(radius - (filled ? 0 : stroke_width));
-
                 float alpha = 0.0f;
                 if (filled)
                 {

the-swift-collective/imgui-cxx

imgui_draw.cpp
@@ -1417,6 +1417,7 @@ void ImDrawList::AddLine(const ImVec2& p1, const ImVec2& p2, ImU32 col, float th
 // flags should contains a mask indicating which corners should be drawn rounded
 // Returns true if the rectangle was drawn, false for some reason it couldn't
 // be (in which case the caller should try again with the regular path drawing API)
+// We are using the textures generated by ImFontAtlasBuildRenderRoundCornersTexData()
 inline bool AddRoundCornerRect(ImDrawList* draw_list, const ImVec2& a, const ImVec2& b, ImU32 col, float rounding, ImDrawFlags flags, bool fill)
 {
 #if 1
@@ -1692,13 +1693,8 @@ void ImDrawList::AddRect(const ImVec2& p_min, const ImVec2& p_max, ImU32 col, fl
 
     // Try to use fast path if we can
     if (rounding > 0)
-    {
         if (AddRoundCornerRect(this, p_min, p_max, col, rounding, flags, /* fill */ false))
-        {
-            // Fast path handled this
             return;
-        }
-    }
 
     if (Flags & ImDrawListFlags_AntiAliasedLines)
         PathRect(p_min + ImVec2(0.50f, 0.50f), p_max - ImVec2(0.50f, 0.50f), rounding, flags);
@@ -1727,14 +1723,10 @@ void ImDrawList::AddRectFilled(const ImVec2& p_min, const ImVec2& p_max, ImU32 c
     {
         // Try fast path first
         if (AddRoundCornerRect(this, p_min, p_max, col, rounding, flags, /* fill */ true))
-        {
             return;
-        }
-        else
-        {
-            PathRect(p_min, p_max, rounding, flags);
-            PathFillConvex(col);
-        }
+
+        PathRect(p_min, p_max, rounding, flags);
+        PathFillConvex(col);
     }
 }
 
@@ -1810,8 +1802,7 @@ inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, fl
     IM_ASSERT(tex_id == draw_list->_TextureIdStack.back());  // Use high-level ImGui::PushFont() or low-level ImDrawList::PushTextureId() to change font.
 
     const int rad = (int)radius;
-    if ((rad < 1) || // Radius 0 will cause issues with the UV lookup below
-        (rad > data->Font->ContainerAtlas->RoundCornersMaxSize))
+    if (rad < 1 || rad > data->Font->ContainerAtlas->RoundCornersMaxSize) // Radius 0 will cause issues with the UV lookup below
         return false; // We can't handle this
 
     // Debug command to force this render path to only execute when shift is held
@@ -1843,7 +1834,6 @@ inline bool AddRoundCornerCircle(ImDrawList* draw_list, const ImVec2& center, fl
 
     // Some useful constants for our calculations
     const float half_sqrt_two = 0.70710678f; // sqrtf(2.0f) * 0.5f
-    const float sqrt_two_minus_one = 0.41421356f; // sqrt(2.0f) - 1.0f
     const float width_offset_parametric = line_width / rad; // Line width in our parametric coordinate space
 
     const int num_verts = fill ? 9 : 16; // Number of vertices we are going to write
@@ -3728,7 +3718,11 @@ static void ImFontAtlasBuildRegisterRoundCornersCustomRects(ImFontAtlas* atlas)
     const int pad = FONT_ATLAS_ROUNDED_CORNER_TEX_PADDING;
     const int max = atlas->RoundCornersMaxSize;
     for (int n = 0; n < max; n++)
-        atlas->RoundCornersRectIds.push_back(atlas->AddCustomRectRegular(n + 1 + pad * 2, n + 1 + FONT_ATLAS_ROUNDED_CORNER_TEX_CENTER_PADDING  + pad * 2));
+    {
+        const int width = n + 1 + pad * 2;
+        const int height = n + 1 + FONT_ATLAS_ROUNDED_CORNER_TEX_CENTER_PADDING + pad * 2;
+        atlas->RoundCornersRectIds.push_back(atlas->AddCustomRectRegular(width, height));
+    }
 }
 
 // Generate the actual pixel data for rounded corners in the atlas
@@ -3781,7 +3775,6 @@ static void ImFontAtlasBuildRenderRoundCornersTexData(ImFontAtlas* atlas)
                 }
 
                 const float dist = ImSqrt((float)(cx*cx+cy*cy)) - (float)(radius - (filled ? 0 : stroke_width));
-
                 float alpha = 0.0f;
                 if (filled)
                 {